72859b51040411204356d1513bcb197d32219912
Andrey Kotlyarov <kotlyarov.andrey01@gmail.com>
2023-07-01 23:32:29 +0900

    feature: ssh

--- frontend/src/utils/consts.js
+++ frontend/src/utils/consts.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://api.mestoproject.nomoreparties.sbs/";
+const baseUrl = "https://api.mestoproject.nomoreparties.sbs";
 // const baseUrl = "http://localhost:3000";
 export default baseUrl;
